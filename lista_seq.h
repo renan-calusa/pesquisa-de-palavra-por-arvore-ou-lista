b3de5611bc4e8f323d654327f65b2f8aef1f2315
Renan Calusa <63789983+renan-calusa@users.noreply.github.com>
2022-12-31 20:18:45 -0300

    Update lista_seq.h

--- lista_seq.h
+++ lista_seq.h
@@ -1,66 +1,89 @@
 #define FALSE 0
 #define TRUE 1
 
-typedef char* Elemento;
+
 typedef int Boolean;
 
-typedef struct {
+typedef struct aux_modulo {
 
-	Elemento* elemento;
+	char* word;
+	int count;
+	int* linha;
+
+} Modulo;
+
+
+typedef struct aux_matriz {
+
 	int livre;
-	int capacidade;
+	int tamanho;
+	Modulo** lista_de_modulos;
 
-} ListaSequencial;
+} Matriz;
 
 
-ListaSequencial* cria_lista(int capacidade){
+Matriz* cria_matriz(int tamanho_matriz, int quantidade_linha) {
 
-	ListaSequencial * lista =  (ListaSequencial *) malloc (sizeof(ListaSequencial));
-	lista->elemento = (Elemento*) malloc (capacidade * sizeof(Elemento));
-	lista->capacidade = capacidade;
-	lista->livre = 0;
+	Matriz* matriz = malloc(sizeof(Matriz)*tamanho_matriz);
 
-	return lista;
+	matriz->tamanho = tamanho_matriz;
+	matriz->livre = 0;
+	matriz->lista_de_modulos = (Modulo**) malloc(sizeof(Modulo*)*tamanho_matriz);
+
+	for(int i=0; i<tamanho_matriz; i++) {
+		
+		matriz->lista_de_modulos[i] = (Modulo*) malloc(sizeof(Modulo));
+		matriz->lista_de_modulos[i]->word = (char*) malloc(sizeof(char)*20);
+		matriz->lista_de_modulos[i]->count = 0;
+		matriz->lista_de_modulos[i]->linha = (int*) malloc(sizeof(int)*quantidade_linha);
+		// prevenir lixo de memoria
+		for(int j=0; j<quantidade_linha; j++) matriz->lista_de_modulos[i]->linha[j] = -1;
+	}
+
+	return matriz;
 }
 
 
-int busca(ListaSequencial* lista, Elemento e){
-
-	// busca binária!
+Boolean busca(Matriz* matriz, char* palavra) {
 
 	int ini = 0;
-	int fim = lista->livre - 1;
+	int fim = matriz->livre - 1;
 	int meio;
 
 	while(ini <= fim){
 
 		meio = (ini + fim) / 2;
 
-		if (strcmp(e, lista->elemento[meio]) == 0) return meio;
-		if (strcmp(e, lista->elemento[meio]) > 0) ini = meio + 1;
-		if (strcmp(e, lista->elemento[meio]) < 0) fim = meio - 1;
+		if (strcmp(palavra, matriz->lista_de_modulos[meio]->word) == 0) return meio;
+		if (strcmp(palavra, matriz->lista_de_modulos[meio]->word) > 0) ini = meio + 1;
+		if (strcmp(palavra, matriz->lista_de_modulos[meio]->word) < 0) fim = meio - 1;
 	}
 		
 	return -1;
 }
 
 
-Boolean insere(ListaSequencial* lista, Elemento e){
+Boolean insere(Matriz* matriz, char* palavra){
 
-	int i;
+	// Verificar, numa lista nao vazia, a existencia de elemento repetido
+	if ((matriz->livre != 0) && (busca(matriz, palavra) != -1)) {
+				
+		// Colocar a linha da palavra repetida na lista de linhas da palavra (...)
+		return FALSE;
+	}
 
 
-	if(lista->livre < lista->capacidade) {
+	if(matriz->livre < matriz->tamanho) {
 
-		for(i = lista->livre; i > 0 && (strcmp(lista->elemento[i - 1], e) > 0); i--) {
+		int i;
 
-			if ((lista->livre != 0) && (busca(lista, e) != -1)) return FALSE;
+		for(i=matriz->livre; i > 0 && (strcmp(matriz->lista_de_modulos[i - 1]->word, palavra) > 0); i--) {
 
-			lista->elemento[i] = lista->elemento[i - 1];
+			matriz->lista_de_modulos[i]->word = matriz->lista_de_modulos[i-1]->word;
 		}
 
-		lista->elemento[i] = e;
-		lista->livre++;
+		matriz->lista_de_modulos[i]->word = palavra;
+		matriz->livre++;
 
 		return TRUE;
 	}
@@ -69,23 +92,14 @@ Boolean insere(ListaSequencial* lista, Elemento e){
 }
 
 
-
-void imprime_lista(ListaSequencial * lista){
-
-	int i;
+void imprime_matriz(Matriz* matriz){
 
 	printf("Lista:");
 
-	for(i = 0; i < lista->livre; i++){
+	for(int i = 0; i < matriz->livre; i++){
 
-		printf(" %s", lista->elemento[i]);
+		printf(" %s", matriz->lista_de_modulos[i]->word);
 	}
 
 	printf("\n");
-}
-
-void destroi_lista(ListaSequencial* lista){
-
-	free(lista->elemento);
-	free(lista);
 }
